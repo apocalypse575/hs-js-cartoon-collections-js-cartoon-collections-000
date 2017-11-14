a9280d93edfae8bfe7dd04b8723ed60daea929c9
apocalypse575 <apocalypse575@gmail.com>
2017-11-14 18:39:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 function dwarfRollCall(dwarves) {
+  let arr = []
+  for(let i = 0, l = dwarves.length; i < l; i++) {
+    arr.push(`${i + 1}. ${dwarves[i]} `)
+  }
+  return arr.join(``)
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  let arr = []
+  for(let i = 0, l = planeteerCalls.length; i < l; i++) {
+  arr.push(`${planeteerCalls[i].toUpperCase()}!`)
+  }
+  return arr
 }
 
 function longPlaneteerCalls(words) {
+  for(let i = 0, l = words.length; i < l; i++) {
+    if (words[i].length <= 4) {
+      return false
+    }
+    else {
+      return true
+    }
+  }
 }
 
-function findTheCheese (foods) {
+function findTheCheese(foods) {
+  let cheese = [`cheddar`, `gouda`, `camembert`]
+  for(let i = 0, l = foods.length; i < l; i++) {
+
+    for(let j = 0, l = cheese.length; j < l; j++) {
+      if(foods[i] === cheese[j]) {
+        return cheese[j]
+      }
+    }
+
+  }
+  return 'no cheese!'
 }
